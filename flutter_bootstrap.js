ecf3b0922f53e3dde0973ff3f080d8211b516b63
Hamed Abbasi <h.abbasi@farazware.com>
2025-03-10 20:41:51 +0330

    change url

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2570717779"
+    serviceWorkerVersion: "3541917350"
   }
 });
